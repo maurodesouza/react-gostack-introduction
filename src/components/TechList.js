0860be7e3407fafb5b96801123564febb198c792
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-14 13:39:34 -0300

    class 07 - removing item from state

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -23,11 +23,19 @@ class ListTech extends Component {
     });
   }
 
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+          {this.state.techs.map(tech => (
+            <li key={tech}>{tech}
+              <button type="button" onClick={() => this.handleDelete(tech)}> Remover </button>
+            </li>
+          ))}
         </ul>
         <input type="text" onChange={this.handleInputChange} value={this.state.newTech}/>
         <button type="submit"> Enviar </button>
