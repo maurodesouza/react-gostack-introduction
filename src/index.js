cf0c57f3b0fdb043458cb10f516ec8d122dfa7f3
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-14 10:48:23 -0300

    class 02 - first component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const teste = (a, b) => a + b;
+import React from 'react';
+import { render } from 'react-dom';
 
-alert(teste(1, 0));
+import App from './App'
+
+render(<App />, document.querySelector('#app'))
