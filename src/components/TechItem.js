e32d19386924708c339c0dfd1c621132512a47f3
Mauro De Souza <maurodesouza2017@hotmail.com>
2020-02-14 15:04:34 -0300

    class 09 - default props and prop types

--- src/components/TechItem.js
+++ src/components/TechItem.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const TechItem = ({ tech, onDelete }) => (
   <li>{tech}
     <button type="button" onClick={onDelete}> Remover </button>
   </li>
 )
+
+TechItem.defaultProps = {
+  tech: 'Oculto'
+}
+
+TechItem.propTypes = {
+  tech: PropTypes.string,
+  onDelete: PropTypes.func.isRequired,
+}
 
 export default TechItem;
